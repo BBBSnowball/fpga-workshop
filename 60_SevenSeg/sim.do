b917d7ccbffd75a99a15b44ef795447c14db1e7c
Benjamin Koch <bbbsnowball@gmail.com>
2019-06-22 04:49:25 +0200

    add button press to simulation

--- 60_SevenSeg/sim.do
+++ 60_SevenSeg/sim.do
@@ -2,6 +2,6 @@ catch {vlib work}
 vcom sevenseg.vhd sevenseg_top.vhd testbench.vhd
 vsim work.testbench
 configure wave -signalnamewidth 1  ;# show only signal name
-add wave /testbench/clk /testbench/DUT/sevenseg_segment /testbench/DUT/sevenseg_digit
+add wave /testbench/clk /testbench/DUT/buttons(4) /testbench/DUT/sevenseg_segment /testbench/DUT/sevenseg_digit
 view wave
-run 1 us
+run 1 ms
--- 60_SevenSeg/testbench.vhd
+++ 60_SevenSeg/testbench.vhd
@@ -6,11 +6,12 @@ end entity;
 
 architecture RTL of testbench is
   signal clk : std_logic := '0';
+  signal buttons : std_logic_vector(2 to 5) := (others => '1');
 begin
   DUT : entity work.sevenseg_top
     port map (
       clk => clk,
-      buttons => (others => '1'),
+      buttons => buttons,
       switches => (others => '1')
     );
 
@@ -18,5 +19,14 @@ begin
   begin
     wait for 10 ns;
     clk <= not(clk);
+  end process;
+  
+  test : process
+  begin
+    wait for 10 us;
+    buttons(4) <= '0';
+    wait for 1 us;
+    buttons(4) <= '1';
+    wait;
   end process;
 end architecture;
