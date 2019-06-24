b5a6600520d00dd0c0019f09e29e4f1455a98a7c
Benjamin Koch <bbbsnowball@gmail.com>
2019-06-24 05:49:35 +0200

    add leds and uart to Qsys

--- 80_SoftCore_SPOILER_SOLUTION/softcore_top.vhd
+++ 80_SoftCore_SPOILER_SOLUTION/softcore_top.vhd
@@ -9,13 +9,17 @@ entity softcore_top is
     clk : in std_logic;
     buttons : in std_logic_vector(2 to 5);
     switches : in std_logic_vector(1 to 8);
+    leds : out std_logic_vector(3 to 14);
     sevenseg_segment : out std_logic_vector(7 downto 0);
-    sevenseg_digit   : out std_logic_vector(7 downto 0)
+    sevenseg_digit   : out std_logic_vector(7 downto 0);
+    uart_sin : in std_logic;
+    uart_sout : out std_logic
   );
 end entity softcore_top;
 
 architecture RTL of softcore_top is
   signal cnt : std_logic_vector(35 downto 0) := (others => '0');
+  signal resetn : std_logic := '0';
   signal sevenseg_data : std_logic_vector(63 downto 0) := (0 => '1', others => '0');
 
     component softcore is
@@ -24,7 +28,10 @@ architecture RTL of softcore_top is
             reset_reset_n : in  std_logic                     := 'X'; -- reset_n
             pio0_export   : out std_logic_vector(31 downto 0);        -- export
             pio1_export   : out std_logic_vector(31 downto 0);        -- export
-            pio2_export   : in  std_logic_vector(11 downto 0) := (others => 'X')  -- export
+            pio2_export   : in  std_logic_vector(11 downto 0) := (others => 'X');  -- export
+            pio3_export   : out std_logic_vector(11 downto 0);       -- export
+            uart_rxd      : in  std_logic                     := 'X';             -- sin
+            uart_txd      : out std_logic                                         -- sout
         );
     end component softcore;
 begin
@@ -32,6 +39,11 @@ begin
   begin
     if rising_edge(clk) then
       cnt <= std_logic_vector(unsigned(cnt)+1);
+      if buttons(2)='0' then
+        resetn <= '0';
+      elsif cnt(10)='1' then
+        resetn <= '1';
+      end if;
     end if;
   end process;
 
@@ -47,9 +59,12 @@ begin
   qsys_system : component softcore
     port map (
       clk_clk       => clk,
-      reset_reset_n => '1',
+      reset_reset_n => resetn,
       pio0_export   => sevenseg_data(31 downto 0),
       pio1_export   => sevenseg_data(63 downto 32),
-      pio2_export   => buttons & switches
+      pio2_export   => buttons & switches,
+      pio3_export   => leds,
+      uart_rxd      => uart_sin,
+      uart_txd      => uart_sout
     );
 end architecture;
--- 80_SoftCore_SPOILER_SOLUTION/timing.sdc
+++ 80_SoftCore_SPOILER_SOLUTION/timing.sdc
@@ -1,7 +1,9 @@
 create_clock -period 20 [get_ports clk]
 set_clock_uncertainty -to [get_clocks clk] 0.5
 set_false_path -from [get_ports {buttons[*] switches[*]}]
-set_output_delay -clock clk 5 [get_ports {sevenseg_segment[*] sevenseg_digit[*]}]
+set_output_delay -clock clk 5 [get_ports {sevenseg_segment[*] sevenseg_digit[*] leds[*]}]
+set_input_delay -clock clk 5 [get_ports {uart_sin}]
+set_output_delay -clock clk 5 [get_ports {uart_sout}]
 
 set_input_delay  5 -clock altera_reserved_tck [get_ports {altera_reserved_tdi altera_reserved_tms}]
 set_output_delay 5 -clock altera_reserved_tck [get_ports {altera_reserved_tdo}]
